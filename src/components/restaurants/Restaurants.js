07203d75a08093d753ff7905d2cafd74e5d122f2
John Ratana <jratana1@gmail.com>
2021-03-11 23:28:56 -0500

    done

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -6,7 +6,6 @@ class Restaurants extends Component {
 
   render() {
     let restaurants = this.props.restaurants.map(restaurant => <Restaurant key={restaurant.id} restaurant={restaurant} deleteRestaurant={this.props.deleteRestaurant}/>)
-
     return(
       <ul>
         {restaurants}
